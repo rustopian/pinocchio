096a0665b2aa8bdc4e5783a63be9d86af9b68293
Nagaprasad V R <81755170+Nagaprasadvr@users.noreply.github.com>
2025-08-13 14:22:55 +0530

    `feat` : Add InitMultisig ixs (#197)
    * add init multisig ix
    
    * chore : resolve comments and refactor

---
+++ programs/token/src/instructions/initialize_multisig_2.rs
@@ -0,0 +1,94 @@
+use core::{mem::MaybeUninit, slice};
+
+use pinocchio::{
+    account_info::AccountInfo,
+    cpi::invoke_with_bounds,
+    instruction::{AccountMeta, Instruction},
+    program_error::ProgramError,
+    ProgramResult,
+};
+
+use crate::instructions::MAX_MULTISIG_SIGNERS;
+
+/// Initialize a new Multisig.
+///
+/// ### Accounts:
+///   0. `[writable]` The multisig account to initialize.
+///   1. ..`1+N`. `[]` The N signer accounts, where N is between 1 and 11.
+pub struct InitializeMultisig2<'a, 'b>
+where
+    'a: 'b,
+{
+    /// Multisig Account.
+    pub multisig: &'a AccountInfo,
+    /// Signer Accounts
+    pub signers: &'b [&'a AccountInfo],
+    /// The number of signers (M) required to validate this multisignature
+    /// account.
+    pub m: u8,
+}
+
+impl InitializeMultisig2<'_, '_> {
+    #[inline(always)]
+    pub fn invoke(&self) -> ProgramResult {
+        let &Self {
+            multisig,
+            signers,
+            m,
+        } = self;
+
+        if signers.len() > MAX_MULTISIG_SIGNERS {
+            return Err(ProgramError::InvalidArgument);
+        }
+
+        let num_accounts = 1 + signers.len();
+
+        // Account metadata
+        const UNINIT_META: MaybeUninit<AccountMeta> = MaybeUninit::<AccountMeta>::uninit();
+        let mut acc_metas = [UNINIT_META; 1 + MAX_MULTISIG_SIGNERS];
+
+        unsafe {
+            // SAFETY:
+            // - `account_metas` is sized to 1 + MAX_MULTISIG_SIGNERS
+            // - Index 0 is always present
+            acc_metas
+                .get_unchecked_mut(0)
+                .write(AccountMeta::writable(multisig.key()));
+        }
+
+        for (account_meta, signer) in acc_metas[1..].iter_mut().zip(signers.iter()) {
+            account_meta.write(AccountMeta::readonly(signer.key()));
+        }
+
+        // Instruction data layout:
+        // -  [0]: instruction discriminator (1 byte, u8)
+        // -  [1]: m (1 byte, u8)
+        let data = &[19, m];
+
+        let instruction = Instruction {
+            program_id: &crate::ID,
+            accounts: unsafe { slice::from_raw_parts(acc_metas.as_ptr() as _, num_accounts) },
+            data,
+        };
+
+        // Account info array
+        const UNINIT_INFO: MaybeUninit<&AccountInfo> = MaybeUninit::uninit();
+        let mut acc_infos = [UNINIT_INFO; 1 + MAX_MULTISIG_SIGNERS];
+
+        unsafe {
+            // SAFETY:
+            // - `account_infos` is sized to 1 + MAX_MULTISIG_SIGNERS
+            // - Index 0 is always present
+            acc_infos.get_unchecked_mut(0).write(multisig);
+        }
+
+        // Fill signer accounts
+        for (account_info, signer) in acc_infos[1..].iter_mut().zip(signers.iter()) {
+            account_info.write(signer);
+        }
+
+        invoke_with_bounds::<{ 1 + MAX_MULTISIG_SIGNERS }>(&instruction, unsafe {
+            slice::from_raw_parts(acc_infos.as_ptr() as _, num_accounts)
+        })
+    }
+}
